7a18ce623cd528980db3dbfe5e028ade8e8f8885
yilmazakin93 <34316968+yilmazakin93@users.noreply.github.com>
2020-07-13 15:20:53 +0300

    styling and debugging

--- my-tutorial/src/Person.js
+++ my-tutorial/src/Person.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import './Person.css';
 
+import classes from './Person.css';
 
-const person = (props) => {
+const person = ( props ) => {
+    const rnd = Math.random();
 
-
+    if ( rnd > 0.7 ) {
+        throw new Error( 'Something went wrong' );
+    }
     return (
-        <div className='Person'>
-            
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type='text'  onChange={props.changed}/>
-        
-        
-        
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
-    
-
-        
-    ) 
+    )
 };
 
 export default person;
